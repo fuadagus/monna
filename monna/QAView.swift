c8b6ee82f5b391a2a528f55798de0b13725bc9f5
Fuad Agus Salim <fuadagussalim@mail.ugm.ac.id>
2026-04-21 13:25:55 +0700

    Create QA View

--- monna/QAView.swift
+++ monna/QAView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct QAView: View {
+    
+    @State private var question:String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading){
+            Text("Python").frame(width: 80, height: 20, alignment: Alignment(horizontal: .center, vertical: .top))
+                .foregroundColor(Color.blue)
+                .bold()
+                .padding(EdgeInsets(top: 2, leading: 16, bottom: 2, trailing: 16))
+                .background{
+                RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(Color.white)
+            }
+            
+            Text("How to make a questionasdfadfa aksdjnfkajdf akjsdfnakjdnf aksdjfnjkdfn akjdfnajdf akjfnjfdna akjfnjf ksjdfnjkf akjdnfjkf akdjnf?").frame(maxWidth: .infinity, maxHeight: 80, alignment: Alignment(horizontal: .leading, vertical: .top)).foregroundColor(Color.black).font(.title).bold().minimumScaleFactor(0.1)
+            HStack{
+                TextField(
+                    "Type your solution here!",
+                    text: $question
+                )
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                .border(.secondary).foregroundColor(Color.white)
+                
+                Button{
+                    print("Tes Tombol")
+                } label: {
+                    Image(systemName: "paperplane")
+                }
+            }
+        }.frame(width: .infinity, height: .infinity, alignment: Alignment(horizontal: .leading, vertical: .top)).multilineTextAlignment(.leading).padding(10).background(Color.qaBackgroundColorOrange).clipShape( RoundedRectangle(cornerRadius: 10))
+        
+        
+        
+        
     }
 }
 
